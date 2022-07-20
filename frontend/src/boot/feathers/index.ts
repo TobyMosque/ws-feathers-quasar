4e62e1874e60eaafeb8340ba877c8f176d5ddb59
Tobias Mesquita <tobias.mesquita@gmail.com>
2022-07-20 10:55:51 -0300

    cookies auth

--- frontend/src/boot/feathers/index.ts
+++ frontend/src/boot/feathers/index.ts
@@ -2,6 +2,9 @@ import { InjectionKey, App as AppVue } from 'vue'
 import { Application, Service } from '@feathersjs/feathers'
 import feathers from '@feathersjs/feathers'
 import { EntityModel as Entity } from 'backend/src/services/entities/entities.model'
+import { Cookies } from 'quasar';
+import { QSsrContext } from '@quasar/app-vite';
+import auth from '@feathersjs/authentication-client';
 
 export type EntityModel = Entity
 
@@ -15,10 +18,23 @@ export type ConfigureFn = Parameters<App['configure']>[0]
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export function bootstrap(app: AppVue, configure: ConfigureFn) {
-  const api = feathers() as App
+export function bootstrap(app: AppVue, ssrContext: QSsrContext, configure: ConfigureFn) {
+  const cookies = process.env.SERVER ? Cookies.parseSSR(ssrContext) : Cookies;
 
+  const api = feathers() as App
+  
   api.configure(configure)
+  api.configure(auth({ 
+    storage: {
+      getItem(key: string) {
+        return JSON.stringify(cookies.get(key));
+      },
+      setItem(key: string, value: string) {
+        const obj = JSON.parse(value);
+        cookies.set(key, obj, { path: '/', sameSite: 'Lax', secure: true });
+      },
+    }
+  }))
   const entityApi = api.service('/api/entity')
 
   app.provide(apiKey, api)
--- frontend/src/boot/feathers/server.ts
+++ frontend/src/boot/feathers/server.ts
@@ -4,6 +4,6 @@ import { bootstrap } from './index';
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({ app }) => {
+export default boot(async ({ app, ssrContext }) => {
   bootstrap(app, services as never)
 })
